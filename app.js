543a988edfae51df64eaff289ec06cc391ef8ce7
bayfamily2020 <bayfamily2020@gmail.com>
2026-08-17 15:51:36 -0700

    将151改为葡萄牙亚速尔群岛

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "world-travel-500-progress-v1";
-const MEDIA_CACHE_KEY = "world-travel-650-media-v5";
+const MEDIA_CACHE_KEY = "world-travel-650-media-v6";
 const $ = (id) => document.getElementById(id);
 let places = [];
 let visited = new Set();
@@ -14,6 +14,7 @@ function escapeHtml(value) {
 }
 
 const featuredMedia = {
+  151:{city:"Ponta Delgada",file:"Sao Miguel Island (Azores). Sete Cidades Caldera, Lagoa de Santiago, Azores.jpg"},
   563:{city:"Tenerife",file:"Lightning and the Milky way.jpg"},
   594:{city:"Chamonix",file:"A view of Mont Blanc from the Tour du Mont Blanc, 2007.jpg"},
   551:{city:"Black Rock City",file:"BM 2010 Centre Camp.jpg"},
